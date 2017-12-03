f9afa56601fb620e9362b64d8a7afc561f95ad0c
Hugo <hugowangchn@gmail.com>
2017-12-03 21:59:07 +1300

    bug fix

--- js/app.js
+++ js/app.js
@@ -1,58 +1,213 @@
 /*
  * Create a list that holds all of your cards
  */
-var cardHolder = [];
-var cardOpen = null;
-var move = 0;
-var nomatch = false;
-var Card = function (cardContent) {
-    this.status = 0; //status of card: 0, close; 1, open; 2, match; 3, no match
-    this.content = cardContent;
-}
-
-Card.prototype.click = function () {
-
-    if (this.status == 0){
-        move++;
-        this.match();
+"use strict";
+class Card{
+    constructor(cardContent,game){
+        this.status = 0; //status of card: 0, close; 1, open; 2, match; 3, no match
+        this.content = cardContent;
+        this.game = game;
     }
-
-}
-
-Card.prototype.match = function(){
-    if (!cardOpen){
-        cardOpen = this;
-        this.status=1;
-    }else{
-        if (cardOpen.content === this.content){
-            cardOpen.status=2;
-            this.status=2;
-        }else{
-            cardOpen.status=3;
-            this.status=3;
-            nomatch = true;
+    click(){
+        if (this.status === 0){
+            this.game.move++;
+            this.match();
         }
-        cardOpen = null;
+    }
+
+    match(){
+        if (this.game.cardOpen===null){
+            this.game.cardOpen = this;
+            this.status=1;
+        }else{
+            if (this.game.cardOpen.content === this.content){
+                this.game.cardOpen.status=2;
+                this.status=2;
+            }else{
+                this.game.cardOpen.status=3;
+                this.status=3;
+                this.game.nomatch = true;
+            }
+            this.game.cardOpen = null;
+        }
+    }
+    toHtml(){
+        let template = `<li class="card" data-position="p_0"><i class="fa fa-${this.content}"></i></li>`;
+        switch (this.status){
+            case 0:
+                break;
+            case 1:
+                template = template.replace('card','card open show');
+                break;
+            case 2:
+                template = template.replace('card','card match');
+                break;
+            case 3:
+                template = template.replace('card','card nomatch animated rubberBand');
+                break;
+        }
+        return template;
+    }
+
+}
+class Game{
+    constructor(){
+        this.cardHolder = [];
+        this.cardOpen = null;
+        this.move = 0;
+        this.nomatch = false;
+        this.time = 0;
+        this.timerid = null;
+    }
+
+    start() {
+        var types = ['diamond','paper-plane-o','anchor','bolt','cube','leaf','bicycle','bomb'];
+        this.move = 0;
+        this.time = 0;
+        this.cardHolder = [];
+        this.cardOpen = null;
+        for(let i=0;i<types.length;i++){
+            this.cardHolder.push(new Card(types[i],this));
+            this.cardHolder.push(new Card(types[i],this));
+        }
+        this.cardHolder = this.shuffle(this.cardHolder);
+        this.timerid = setInterval(()=>{
+            this.reloadTimer();
+        },1000);
+        this.reloadCards();
+
+    }
+
+    playAgain() {
+        document.getElementsByClassName('result')[0].style.display="none";
+        document.getElementsByClassName('container')[0].style.display="flex";
+        this.start();
+    }
+
+    timer(){
+
+    }
+
+    // Shuffle function from http://stackoverflow.com/a/2450976
+    shuffle(array) {
+        let currentIndex = array.length, temporaryValue, randomIndex;
+
+        while (currentIndex !== 0) {
+            randomIndex = Math.floor(Math.random() * currentIndex);
+            currentIndex -= 1;
+            temporaryValue = array[currentIndex];
+            array[currentIndex] = array[randomIndex];
+            array[randomIndex] = temporaryValue;
+        }
+
+        return array;
+    }
+
+
+
+    calculateScore() {
+        let score = 3;
+        let t = this.move-20;
+        t = t>0?t:0;
+        score = score - t/5;
+        score = score<1?1:score;
+        return score;
+    }
+
+    calculateTime(){
+        let s = this.time%60;
+        let m = (this.time-s)/60;
+        return [s,m];
+    }
+
+    reloadTimer(){
+        this.time++;
+        let [s,m] = this.calculateTime();
+        document.getElementsByClassName('timer')[0].innerHTML = `${m} m ${s} s`;
+    }
+
+    reloadScore() {
+        let scoreContent = '';
+        let t = this.calculateScore();
+        for(let i=t;i>0;i--){
+            if (i>=1){
+                scoreContent += '<li><i class="fa fa-star"></i></li>';
+            }else{
+                scoreContent += '<li><i class="fa fa-star-half-o"></i></li>';
+            }
+        }
+        if(t<=0){
+            scoreContent += '<li><i class="fa fa-star-o"></i></li>';
+            scoreContent += '<li><i class="fa fa-star-o"></i></li>';
+            scoreContent += '<li><i class="fa fa-star-o"></i></li>';
+        }else if(t<=1){
+            scoreContent += '<li><i class="fa fa-star-o"></i></li>';
+            scoreContent += '<li><i class="fa fa-star-o"></i></li>';
+        }else if(t<=2){
+            scoreContent += '<li><i class="fa fa-star-o"></i></li>';
+        }
+        document.getElementsByClassName('stars')[0].innerHTML = scoreContent;
+        //reload the move
+        document.getElementsByClassName('moves')[0].innerHTML = this.move;
+    }
+
+    /**
+     * Reload the html of cards
+     */
+    reloadCards() {
+        if(this.success()){
+            clearInterval(this.timerid)
+            let [s,m] = this.calculateTime();
+            let result = `With ${this.move} Moves and ${this.calculateScore()} Stars in ${m} Min ${s} Second` ;
+            document.getElementById('result').innerHTML = result;
+            document.getElementsByClassName('container')[0].style.display="none";
+            document.getElementsByClassName('result')[0].style.display="flex";
+            return;
+        }
+        //reload the cards html
+        let content = '';
+        for(let i=0;i<this.cardHolder.length;i++){
+            content += this.cardHolder[i].toHtml().replace('p_0',i.toString());
+        }
+        document.getElementById('deck').innerHTML = content;
+        this.reloadScore();
+        //bind listener
+        let cards = document.getElementsByClassName('card');
+        for(let i=0;i<cards.length;i++){
+            cards[i].addEventListener('click',obj=>{
+                // console.log(obj);
+                let p = parseInt(obj.target.getAttribute('data-position'));
+                this.cardHolder[p].click();
+                this.reloadCards();
+            });
+        }
+        if(this.nomatch){
+            this.nomatch = false;
+            setTimeout(()=>{
+                for(let i=0;i<this.cardHolder.length;i++){
+                    if(this.cardHolder[i].status===3){
+                        this.cardHolder[i].status=0;
+                    }
+
+                }
+                this.reloadCards();
+            },800);
+        }
+    }
+    success() {
+        for(let i=0;i<this.cardHolder.length;i++){
+            if(this.cardHolder[i].status!==2){
+                return false;
+            }
+
+        }
+        return true;
     }
 }
 
-Card.prototype.toHtml = function () {
-    var template = '<li class="card" data-position="p_0"><i class="fa fa-'+this.content+'"></i></li>';
-    switch (this.status){
-        case 0:
-            break;
-        case 1:
-            template = template.replace('card','card open show');
-            break;
-        case 2:
-            template = template.replace('card','card match');
-            break;
-        case 3:
-            template = template.replace('card','card nomatch');
-            break;
-    }
-    return template;
-}
+
+
+
 
 /*
  * Display the cards on the page
@@ -61,129 +216,9 @@ Card.prototype.toHtml = function () {
  *   - add each card's HTML to the page
  */
 
-// Shuffle function from http://stackoverflow.com/a/2450976
-function shuffle(array) {
-    var currentIndex = array.length, temporaryValue, randomIndex;
 
-    while (currentIndex !== 0) {
-        randomIndex = Math.floor(Math.random() * currentIndex);
-        currentIndex -= 1;
-        temporaryValue = array[currentIndex];
-        array[currentIndex] = array[randomIndex];
-        array[randomIndex] = temporaryValue;
-    }
 
-    return array;
-}
 
-function reloadScore() {
-    var scoreContent = '';
-    var t = calculateScore();
-    for(i=t;i>0;i--){
-        if (i>=1){
-            scoreContent += '<li><i class="fa fa-star"></i></li>';
-        }else{
-            scoreContent += '<li><i class="fa fa-star-half-o"></i></li>';
-        }
-    }
-    if(t<=0){
-        scoreContent += '<li><i class="fa fa-star-o"></i></li>';
-        scoreContent += '<li><i class="fa fa-star-o"></i></li>';
-        scoreContent += '<li><i class="fa fa-star-o"></i></li>';
-    }else if(t<=1){
-        scoreContent += '<li><i class="fa fa-star-o"></i></li>';
-        scoreContent += '<li><i class="fa fa-star-o"></i></li>';
-    }else if(t<=2){
-        scoreContent += '<li><i class="fa fa-star-o"></i></li>';
-    }
-    document.getElementsByClassName('stars')[0].innerHTML = scoreContent;
-    //reload the move
-    document.getElementsByClassName('moves')[0].innerHTML = move;
-}
-
-function calculateScore() {
-    var score = 3;
-    var t = move-20;
-    t = t>0?t:0;
-    score = score - t/5;
-    score = score<0?0:score;
-    return score;
-}
-
-/**
- * Reload the html of cards
- */
-function reloadCards() {
-    if(success()){
-        var result = 'With '+move+' Moves and '+calculateScore()+' Stars';
-        document.getElementById('result').innerHTML = result;
-        document.getElementsByClassName('container')[0].style.display="none";
-        document.getElementsByClassName('result')[0].style.display="flex";
-        return;
-    }
-    //reload the cards html
-    var content = '';
-    for(i=0;i<cardHolder.length;i++){
-        content += cardHolder[i].toHtml().replace('p_0',i.toString());
-    }
-    document.getElementById('deck').innerHTML = content;
-    reloadScore();
-    //bind listener
-    var cards = document.getElementsByClassName('card');
-    for(i=0;i<cards.length;i++){
-        cards[i].addEventListener('click',function () {
-
-            var p = parseInt(this.getAttribute('data-position'));
-            // console.log(p);
-            cardHolder[p].click();
-            reloadCards();
-        });
-    }
-    if(nomatch){
-        nomatch = false;
-        setTimeout(function () {
-            for(i=0;i<cardHolder.length;i++){
-                if(cardHolder[i].status==3){
-                    cardHolder[i].status=0;
-                }
-
-            }
-            reloadCards();
-        },500);
-    }
-}
-
-function success() {
-    for(i=0;i<cardHolder.length;i++){
-        if(cardHolder[i].status!=2){
-            return false;
-        }
-
-    }
-    return true;
-}
-
-function start() {
-    var types = ['diamond','paper-plane-o','anchor','bolt','cube','leaf','bicycle','bomb'];
-    move = 0;
-    cardHolder = [];
-    cardOpen = null;
-
-    for(i=0;i<types.length;i++){
-        cardHolder.push(new Card(types[i]));
-        cardHolder.push(new Card(types[i]));
-    }
-    cardHolder = shuffle(cardHolder);
-    reloadCards();
-
-}
-
-function playAgain() {
-    document.getElementsByClassName('result')[0].style.display="none";
-    document.getElementsByClassName('container')[0].style.display="flex";
-    start();
-
-}
 /*
  * set up the event listener for a card. If a card is clicked:
  *  - display the card's symbol (put this functionality in another function that you call from this one)
@@ -194,7 +229,8 @@ function playAgain() {
  *    + increment the move counter and display it on the page (put this functionality in another function that you call from this one)
  *    + if all cards have matched, display a message with the final score (put this functionality in another function that you call from this one)
  */
-start();
+const game = new Game();
+game.start();
 
 
 
